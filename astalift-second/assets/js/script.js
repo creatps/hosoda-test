43211d3392a076b6f8d8579ee6a5e490d8c9c5fb
Ayaka Terao <ayaka.terao@creatps.com>
2022-11-15 10:18:59 +0900

    jsscript ハンバーガーアイコンクリックすると動く完成

--- astalift-second/assets/js/script.js
+++ astalift-second/assets/js/script.js
@@ -1,9 +1,17 @@
-const ham = document.querySelector('#js-hamburger');
-const nav = document.querySelector('#js-nav');
+// hamburgerIconというidで要素を取得する
+const hamburgerIcon = document.getElementById('hamburgerIcon');
+const hamburgerLineOne = document.getElementById('hamburgerLineOne');
+const hamburgerLineTwo = document.getElementById('hamburgerLineTwo');
+const hamburgerLineThree = document.getElementById('hamburgerLineThree');
 
-ham.addEventListener('click', function () {
-
-ham.classList.toggle('active');
-nav.classList.toggle('active');
-
-});
+// ハンバーガー押すとこういうアクションを起こす
+hamburgerIcon.addEventListener('click' ,
+    () => {
+        // このidがついている要素にクラスを toggle((トグル)＝ついてたら外すなかったらつける) する
+        hamburgerIcon.classList.toggle('hamburger-icon--open');
+        // hamburgerLineOne に hamburger-icon__line--left のクラスを付与
+        hamburgerLineOne.classList.toggle('hamburger-icon__line--left');
+        hamburgerLineTwo.classList.toggle('hamburger-icon__line--none');
+        hamburgerLineThree.classList.toggle('hamburger-icon__line--right');
+    }
+)
